662e7f6572d8cbcf2f91ada8130992d59f09c392
Dalyclnl <dalyarmijoa@yahoo.com>
2023-07-04 16:04:55 +0000

    people

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { People} from "./views/people";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -24,6 +25,8 @@ const Layout = () => {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
+						<Route path="/people" 
+						element={<People />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
---
+++ src/js/views/people.js
@@ -0,0 +1,38 @@
+import React, {useState,useEffect} from "react";
+
+
+const People = () => {
+    const [people, setPeople] = useState([])
+
+    useEffect (() =>{
+        fectch ('https://www.swapi.tech/api/people')
+        .then((response)=>{
+            return response.json()
+        }).then((response)=>{
+            setPeople(response.result)
+        })
+    }, [])
+
+    return(
+        <div className="container">
+            <div className="row">
+            {
+                people.map((people , index)=>{
+                    return <div key={index}>
+                        <h4>(people.name) </h4>
+                        </div>
+
+                })
+            }
+
+            </div>
+
+        </div>
+
+    )
+
+
+}
+
+
+export default People;
